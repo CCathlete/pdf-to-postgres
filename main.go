44e73e3e37f3155c6ad9473bb56b617aad83361f
CCat <ccathlete01@gmail.com>
2024-04-28 21:30:41 +0200

    Writing to txt file needs fixing, it stays empty.

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"log"
+	"os"
 	pdf2pg "pdf-to-postgres/pdfHandler"
+	"strings"
 )
 
 func main() {
 	docPath := "Parasitology/1 - Introduction to helminthology.pdf"
 	pages := pdf2pg.ParsePdf(docPath)
+	txtPath := strings.Replace(docPath, "pdf", "txt", -1) // -1 means all instances.
+
+	file, err := os.OpenFile(txtPath, os.O_CREATE|os.O_APPEND, 0644)
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("Failed to close the txt file: %v.", err)
+		}
+	}()
+	if err != nil {
+		log.Fatalf("Failed to append or create the txt file: %v.", err)
+	}
+
 	for _, page := range pages {
-		println(page.Content)
+		file.WriteString(page.Content)
 	}
 }
