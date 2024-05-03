2cb4aa17b72874b3315d057d2aca06ff7595b200
CCat <ccathlete01@gmail.com>
2024-05-03 17:43:39 +0200

    Created the function that takes parasite info and puts it into struct. The regex I used isn't good so in the future it'll need more work on extracting better text.

--- pdfHandler/pdfHandler.go
+++ pdfHandler/pdfHandler.go
@@ -5,9 +5,46 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 
 	pdf2txt "github.com/heussd/pdftotext-go"
 )
+
+type ParasiteInfo struct {
+	ScientificName string
+	CommonName     string
+	SizeAdult      string
+	SizeEgg        string
+	Importance     string
+	Diagnosis      string
+	Treatment      string
+	Note           string
+}
+
+func ConvertToText(pdfPath string) {
+	cmd := exec.Command("bash", "-c", "pdftotext "+pdfPath)
+	stdOutErr, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(stdOutErr)
+		log.Fatalf("Failed to run pdftotext using the path %s, the error was: %v\n", pdfPath, err)
+	}
+}
+
+func extractParasitesInfo(txtPath string) (output []ParasiteInfo) {
+	txtBytes, err := os.ReadFile(txtPath)
+	if err != nil {
+		log.Fatalf("Failed to read txt document: %v\n", err)
+	}
+	txtString := string(txtBytes)
+	pattern := `((.*\n*)*)Fig` // prototype pattern, change in the future.
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllString(txtString, -1)
+	for _, match := range matches {
+		pInfo := ParasiteInfo{Note: match}
+		output = append(output, pInfo)
+	}
+	return output
+}
 
 func ParsePdf(pdfPath string) []pdf2txt.PdfPage {
 	pdfBytes, err := os.ReadFile(pdfPath)
@@ -28,13 +65,4 @@ func ParsePdf(pdfPath string) []pdf2txt.PdfPage {
 	}
 
 	return pages
-}
-
-func ConvertToText(pdfPath string) {
-	cmd := exec.Command("bash", "-c", "pdftotext "+pdfPath)
-	stdOutErr, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println(stdOutErr)
-		log.Fatalf("Failed to run pdftotext using the path %s, the error was: %v\n", pdfPath, err)
-	}
 }
