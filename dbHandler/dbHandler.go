eac5212b8f9f125d43147b506de3428806b8260a
CCat <ccathlete01@gmail.com>
2024-05-09 18:21:46 +0200

    Progress with the dbInit func. Need to see why the last bash command fails.

--- dbHandler/dbHandler.go
+++ dbHandler/dbHandler.go
@@ -1,9 +1,11 @@
 package dbHandler
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 
 	_ "github.com/lib/pq"
@@ -24,11 +26,47 @@ func DbInit(dbInfo map[interface{}]interface{}) *sql.DB {
 
 	dbName := dbInfo["name"].(string)
 
-	queryString := fmt.Sprintf(
-		`psql -lqt | cut -d \| -f 1 | grep -qw '%s'; echo $?)`,
-		dbName)
-	exitVal, _ := exec.Command(queryString).Output()
+	reader1, writer1, _ := os.Pipe()
+	reader2, writer2, _ := os.Pipe()
+	outBuff := bytes.Buffer{}
+	//Each writer writes to its reader, they're connected through the pipe.
 
+	cmd1 := exec.Command("/bin/bash", "-c", "psql -lqt")
+	// cmd1 := exec.Command("/bin/bash", "-c", "echo hi")
+	cmd1.Stdout = writer1
+	err = cmd1.Start()
+	if err != nil {
+		log.Fatalf("Issue with starting the command %s: %v\n",
+			cmd1.String(), err)
+	}
+	err = cmd1.Wait()
+	writer1.Close()
+	if err != nil {
+		log.Fatalf("Issue with finishing the command %s: %v\n",
+			cmd1.String(), err)
+	}
+
+	cmd2 := exec.Command("/bin/bash", "-c", `cut -d \| -f 1`)
+	cmd2.Stdin = reader1
+	cmd2.Stdout = writer2
+	err = cmd2.Run()
+	writer2.Close()
+	if err != nil {
+		log.Fatalf("Issue with running the command %s: %v\n",
+			cmd2.String(), err)
+	}
+
+	cmd3String := fmt.Sprintf(`grep -qw '%s'`, dbName)
+	cmd3 := exec.Command("/bin/bash", "-c", cmd3String)
+	cmd3.Stdin = reader2
+	cmd3.Stdout = &outBuff
+	err = cmd3.Run()
+	if err != nil {
+		log.Fatalf("Issue with running the command %s: %v\n",
+			cmd3.String(), err)
+	}
+
+	exitVal := outBuff.String()
 	if len(exitVal) != 0 {
 
 		//If the database doesn't exist, we create it.
